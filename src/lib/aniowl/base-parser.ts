fef18ebfe0ae4931f0e984c7f63b67de0ff4afaf
Origin Cheel <origincheel@gmail.com>
2025-07-16 08:01:44 +0000

    OK now copy all the files that works in the animeowl scrapper to a new f

---
+++ src/lib/aniowl/base-parser.ts
@@ -0,0 +1,14 @@
+
+import BaseProvider from './base-provider';
+import { AxiosAdapter } from 'axios';
+
+abstract class BaseParser extends BaseProvider {
+  /**
+   * Search for books/anime/manga/etc using the given query
+   *
+   * returns a promise resolving to a data object
+   */
+  abstract search(query: string, ...args: any[]): Promise<unknown>;
+}
+
+export default BaseParser;
---
+++ src/lib/aniowl/utils.ts
@@ -0,0 +1,7 @@
+
+export const USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
+
+export const KWIK_COOKIE_NAME = 'kwik_session';
+export const ANIMEPAHE_COOKIE_NAME = 'animepahe_session';
+export const PAHEWIN_COOKIE_NAME = '__ddg1_';
