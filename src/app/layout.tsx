50c8e2e7491201dc3c63bc8f5e63b521bc9e90b5
Origin Cheel <origincheel@gmail.com>
2025-07-16 07:59:29 +0000

    Change the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,37 @@
+
 import type { Metadata } from 'next';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 
+const BotIcon = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M12 8V4H8" />
+  <rect width="16" height="12" x="4" y="8" rx="2" />
+  <path d="M2 14h2" />
+  <path d="M20 14h2" />
+  <path d="M15 13v2" />
+  <path d="M9 13v2" />
+</svg>
+`;
+
+const BotIconDataURL = `data:image/svg+xml;base64,${btoa(BotIcon)}`;
+
 export const metadata: Metadata = {
   title: 'TeleVerse',
   description: 'Your portal to the Telegram universe.',
+  icons: {
+    icon: BotIconDataURL,
+  }
 };
 
 export default function RootLayout({
